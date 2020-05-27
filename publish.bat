e96d322fccbb10f068fcc27e1d54a9489d6cd40a
mss7z <49343918+mss7z@users.noreply.github.com>
2020-05-27 17:37:40 +0900

    fix publish.bat

--- publish.bat
+++ publish.bat
@@ -3,6 +3,9 @@ cd /d %~dp0
 setlocal
 set "exclusionFile=*.bat .gitignore *Template.html robots.txt"
 set "exclusionDir=working .git"
+if exist release\robots.txt (
+	del release\robots.txt
+)
 robocopy . .\release /mir /xf %exclusionFile% /xd %exclusionDir%
 ren .\release\robotsTemplate.txt robots.txt
 endlocal
