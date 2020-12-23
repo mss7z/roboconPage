9003b8b8cde2a34a900eca7c90f0f18a970dabfe
mss7z <49343918+mss7z@users.noreply.github.com>
2020-12-24 07:14:27 +0900

    add old pages

---
+++ old/toggle.js
@@ -0,0 +1,13 @@
+﻿function toggle_body(d){
+	var Target = document.getElementById('body'+d);
+	if(!Target){return;}
+	var CurrClass = Target.getAttribute("class")||Target.getAttribute("className");
+	var CurrStyle = Target.style.display;
+	if (CurrStyle == 'none' || CurrClass.indexOf('toggle_hidden', 0)>=0) {
+		Target.style.display = 'block';
+		var SetClass = CurrClass.replace(/toggle_hidden/, '');
+		Target.setAttribute("class", SetClass, 0)||Target.setAttribute("className", SetClass, 0);
+	} else {
+		Target.style.display = 'none';
+	}
+}
